a7d07c81bca9e963cb89394039f8c83033272643
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-11-24 07:27:59 -0300

    fix: Corrige verificação de usuário logado

--- services/api.ts
+++ services/api.ts
@@ -1,3 +1,5 @@
+import { useAuth } from "@/hooks/useAuth";
+import { storageUserGet } from "@/storage/storageUser";
 import axios from "axios";
 
 const api = axios.create({
@@ -5,5 +7,29 @@ const api = axios.create({
 });
 
 api.defaults.timeout = 5000;
+
+let tokenInMemory: string | null = null;
+
+(async () => {
+  const session = await storageUserGet();
+  if (session) {
+    tokenInMemory = session.token;
+    api.defaults.headers.token = session.token;
+  }
+})();
+
+api.interceptors.request.use(async (config) => {
+  if (tokenInMemory) {
+    config.headers.token = tokenInMemory;
+  } else {
+    const session = await storageUserGet();
+
+    if (session) {
+      tokenInMemory = session.token;
+      config.headers.token = session.token;
+    }
+  }
+  return config;
+});
 
 export { api };
